4e26eb6e983dc794b90eed3301bf9fc5c68152e1
Rodrigo Barros <rodrigobarros2802@gmail.com>
2022-09-16 20:44:30 -0300

    Conclusão do script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
 function mudaImagem (imagem) {
     document.querySelector('.vaporfly').src = imagem;
 }
+
+function mudaCor (color) {
+    const cor = document.querySelector('.circle');
+    cor.style.backgroundColor = color;
+}
